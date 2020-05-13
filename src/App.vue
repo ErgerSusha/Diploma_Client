27e3d16740c72b3aa377ef15f827d3f1f329fabb
ErgerSusha <ergersusha2@gmail.com>
2020-05-14 00:30:19 +0300

    Added call status update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <video id="local-stream" loop autoplay playsinline></video>
-    <video id="remote-stream" loop autoplay playsinline></video>
-    <h3>{{isAdmin ? ' Admin' : 'Client'}}</h3>
+    <div v-if="callIsOn">
+      <video ref="localStream" loop autoplay playsinline></video>
+      <img id="hang-up-button" @click="hangUp()"
+        src="https://lh3.googleusercontent.com/AJdc5MZScZb4Yk6tx_6gjfLReztRaHFujar2CUcnsyf24RAwUW9yLbsAb7h2ZLrpLQ">
+      <video ref="remoteStream" loop autoplay playsinline></video>
+    </div>
+    <h4>{{isAdmin ? ' Admin' : 'Client'}} <i>{{generateNickname(socket.id)}}</i></h4>
     <h5 v-if="users.length">Available {{isAdmin ? ' clients' : ' admins'}}</h5>
     <h5 v-else>Empty, please wait... ⌛</h5>
     <transition-group name="list-complete" tag="div">
-      <div v-for="user in users" :key="user" class="users-button" @click="callUser(user)">{{ user }}</div>
+      <div v-for="user in users"
+        :key="user.id"
+        :class="{'is-calling': user.isCalling }"
+        class="users-button"
+        @click="callUser(user.id)">
+        {{ generateNickname(user.id) }}
+      </div>
     </transition-group>
   </div>
 </template>
@@ -16,34 +26,49 @@ import Vue from "vue";
 import Component from "vue-class-component";
 import io from "socket.io-client";
 
+interface User {
+  id: string;
+  isCalling: boolean;
+}
+
+interface CallStatus {
+  isCalling: boolean;
+  callingAdminId?: string;
+  callingClientId?: string;
+}
+
 @Component
 export default class App extends Vue {
 
-  users: string[] = [];
+  users: User[] = [];
+  opponentId: string;
   isAdmin = false;
-  rtcConnection = new RTCPeerConnection({
-    iceServers: [
-      { urls: "stun:stun3.l.google.com:19302" },
-      { urls: "stun:stun1.l.google.com:19302" }
-    ]
-  });
-  socket!: SocketIOClient.Socket;
+  rtcConnection: RTCPeerConnection;
+  localStream: MediaStream;
+  socket: SocketIOClient.Socket;
+  callIsOn = false;
 
   created() {
-    const rand = Math.random();
-    if (rand > 0.5) {
-      this.socket = io.connect("https://diploma-api.herokuapp.com");
+    if (Math.random() > 0.5) {
+      this.socket = io.connect("http://localhost:3000");
     } else {
-      this.socket = io.connect("https://diploma-api.herokuapp.com", {
+      this.socket = io.connect("http://localhost:3000", {
         query: `adminToken=op9eeftt345d34d`
       });
       this.isAdmin = true;
     }
 
-    this.socket.on("init_users", (users: string[]) => (this.users = users));
-    this.socket.on("user_connected", (userId: string) => this.users.push(userId));
-    this.socket.on("user_disconnected", (userId: string) =>
-      this.users.splice(this.users.indexOf(userId), 1)
+    this.socket.on("init_users", (users: User[]) => {
+      this.users = users;
+    });
+    this.socket.on("user_connected", (id: string) => this.users.push({ id, isCalling: false }));
+    this.socket.on("user_disconnected", (id: string) =>
+      this.users.splice(this.users.findIndex(user => user.id === id), 1)
+    );
+    this.socket.on("user_call_status_changed", (status: CallStatus) =>
+      this.users.find(user =>
+        user.id === (this.isAdmin ? status.callingClientId : status.callingAdminId)
+      ).isCalling = status.isCalling
     );
 
     this.socket.on("offer", (data: string) => this.handleOffer(JSON.parse(data)));
@@ -59,6 +84,7 @@ export default class App extends Vue {
         );
       }
     });
+    this.socket.on('hangup', () => this.closeConnection());
   }
 
   sendRtcEvent(data: {
@@ -72,6 +98,8 @@ export default class App extends Vue {
   }
 
   async callUser(userId: string) {
+    this.callIsOn = true;
+    this.opponentId = userId;
     await this.getMedia(userId);
     const offer = await this.rtcConnection.createOffer();
     await this.rtcConnection.setLocalDescription(offer);
@@ -83,8 +111,10 @@ export default class App extends Vue {
   }
 
   async handleOffer(data: { callerId: string; offer: RTCSessionDescriptionInit }) {
-    const accepted = confirm(data.callerId + " is calling. Answer?");
+    const accepted = confirm(this.generateNickname(data.callerId) + " is calling. Answer?");
     if (accepted) {
+      this.callIsOn = true;
+      this.opponentId = data.callerId;
       await this.getMedia(data.callerId);
       await this.rtcConnection.setRemoteDescription(
         new RTCSessionDescription(data.offer)
@@ -101,19 +131,26 @@ export default class App extends Vue {
   }
 
   async getMedia(calleeId: string) {
+    this.rtcConnection = new RTCPeerConnection({
+      iceServers: [
+        { urls: "stun:stun3.l.google.com:19302" },
+        { urls: "stun:stun1.l.google.com:19302" }
+      ]
+    });
     const stream = await navigator.mediaDevices.getUserMedia({
       video: true,
       audio: true
     });
 
-    (document.getElementById("local-stream") as HTMLVideoElement).srcObject = stream;
+    (this.$refs.localStream as HTMLVideoElement).srcObject = stream;
+    this.localStream = stream;
+
+    this.rtcConnection.ontrack = ev =>
+      (this.$refs.remoteStream as HTMLVideoElement).srcObject = ev.streams[0];
 
     stream
       .getTracks()
       .forEach(track => this.rtcConnection.addTrack(track, stream));
-
-    this.rtcConnection.ontrack = ev =>
-      (document.getElementById("remote-stream") as HTMLVideoElement).srcObject = ev.streams[0];
 
     this.rtcConnection.onicecandidate = event => {
       if (event.candidate) {
@@ -125,16 +162,79 @@ export default class App extends Vue {
       }
     };
   }
+
+  hangUp() {
+    this.sendRtcEvent({
+      type: 'hangup',
+      calleeId: this.opponentId
+    });
+    this.closeConnection();
+  }
+
+  closeConnection() {
+    (this.$refs.remoteStream as HTMLVideoElement).srcObject = null;
+
+    this.localStream.getTracks().forEach(track => track.stop());
+    this.localStream = null;
+    this.rtcConnection.close();
+
+    this.callIsOn = false;
+  }
+
+  generateNickname(userId: string): string {
+    if (!userId) {
+      return '';
+    }
+    let sum = 0;
+    for (let i = 0; i < userId.length; i++) {
+      sum += userId.charAt(i).charCodeAt(0);
+    }
+    switch (sum % 10) {
+      case 0:
+        return 'Funny Koala';
+      case 1:
+        return 'Strange Tiger';
+      case 2:
+        return 'Little Elephant';
+      case 3:
+        return 'Huge Bee';
+      case 4:
+        return 'Purple Lion';
+      case 5:
+        return 'Gorgeous Lizard';
+      case 6:
+        return 'Weird Wolf';
+      case 7:
+        return 'Tiny Buffalo';
+      case 8:
+        return 'Red Bear';
+      case 9:
+        return 'Spanish Tasmanian Devil';
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+body {
+  margin: 0;
+  display: flex;
+  height: 100vh;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+#hang-up-button {
+  border-radius: 50%;
+  width: 5vw;
+  height: 5vw;
+  margin: 0 2vw;
+  cursor: pointer;
 }
 
 .users-button {
@@ -152,18 +252,23 @@ export default class App extends Vue {
   align-items: center;
   cursor: pointer;
   box-shadow: 0 1px 3px rgba(124, 111, 111, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  transition: box-shadow 0.3s cubic-bezier(0.25, 0.8, 0.25, 1),
-    transform 0.5s ease-in-out, opacity 0.5s ease-in-out;
+  transition: box-shadow .3s cubic-bezier(0.25, 0.8, 0.25, 1),
+    transform .5s ease-in-out, opacity .5s ease-in-out, background .25s ease;
 }
 
 .users-button:hover {
   box-shadow: 0 6px 10px rgba(0, 0, 0, 0.25), 0 4px 4px rgba(0, 0, 0, 0.22);
 }
 
+.is-calling {
+  pointer-events: none;
+  background: #999;
+}
+
 .list-complete-enter,
 .list-complete-leave-to {
   opacity: 0;
-  transform: translateY(30px);
+  transform: translateY(50px);
 }
 
 .list-complete-leave-active {
@@ -171,6 +276,6 @@ export default class App extends Vue {
 }
 
 video {
-  max-width: 50%;
+  max-width: 40%;
 }
 </style>
